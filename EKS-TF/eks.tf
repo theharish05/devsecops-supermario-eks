286a1ae65cb763fa21d49de5c350e4ca5d55efd7
theharish05 <m.harish1722005@gmail.com>
2026-07-21 05:08:01 -0400

    feat: finalize mario-v2 cluster access and deployment setup

--- EKS-TF/eks.tf
+++ EKS-TF/eks.tf
@@ -27,3 +27,19 @@ module "eks" {
     Project     = "mario-v2"
   }
 }
+
+resource "aws_eks_access_entry" "github_access" {
+  cluster_name      = module.eks.cluster_name
+  principal_arn     = "arn:aws:iam::823729324830:root"
+  type              = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "cluster_admin" {
+  cluster_name  = module.eks.cluster_name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = aws_eks_access_entry.github_access.principal_arn
+
+  access_scope {
+    type = "cluster"
+  }
+}
